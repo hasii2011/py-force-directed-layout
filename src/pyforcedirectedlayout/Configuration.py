706ef38bc784eb6f2e2c80396bd672ffbe4408f2
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2024-10-26 07:56:34 -0500

    <Refactor>[Configuration]: <No more boiler plates>
    [176 lines vs. 73 a WIN !!!!]
    
    [https://github.com/hasii2011/py-force-directed-layout/issues/18]

--- src/pyforcedirectedlayout/Configuration.py
+++ src/pyforcedirectedlayout/Configuration.py
@@ -1,163 +1,72 @@
 
+from logging import Logger
+from logging import getLogger
+
+from codeallybasic.DynamicConfiguration import DynamicConfiguration
+from codeallybasic.DynamicConfiguration import KeyName
+from codeallybasic.DynamicConfiguration import SectionName
+from codeallybasic.DynamicConfiguration import Sections
+
+from codeallybasic.DynamicConfiguration import ValueDescription
+from codeallybasic.DynamicConfiguration import ValueDescriptions
 from codeallybasic.MinMax import MinMax
+
+from codeallybasic.SecureConversions import SecureConversions
+
 from codeallybasic.SingletonV3 import SingletonV3
-
-from codeallybasic.ConfigurationProperties import ConfigurationNameValue
-from codeallybasic.ConfigurationProperties import ConfigurationProperties
-from codeallybasic.ConfigurationProperties import PropertyName
-from codeallybasic.ConfigurationProperties import Section
-from codeallybasic.ConfigurationProperties import SectionName
-from codeallybasic.ConfigurationProperties import Sections
-from codeallybasic.ConfigurationProperties import configurationGetter
-from codeallybasic.ConfigurationProperties import configurationSetter
-
 
 X_RANGE_MIN: int = -1024
 X_RANGE_MAX: int = 1024
 Y_RANGE_MIN: int = -1024
 Y_RANGE_MAX: int = 1024
 
+arrangeProperties: ValueDescriptions = ValueDescriptions(
+    {
+        KeyName('damping'):         ValueDescription(defaultValue='0.5',   deserializer=SecureConversions.secureFloat),
+        KeyName('springLength'):    ValueDescription(defaultValue='100',   deserializer=SecureConversions.secureInteger),
+        KeyName('maxIterations'):   ValueDescription(defaultValue='500',   deserializer=SecureConversions.secureInteger),
+        KeyName('attractionForce'): ValueDescription(defaultValue='0.1',   deserializer=SecureConversions.secureFloat),
+        KeyName('repulsionForce'):  ValueDescription(defaultValue='10000', deserializer=SecureConversions.secureInteger),
+    }
+)
+
 DEFAULT_MIN_X_COORDINATE: int = -50
 DEFAULT_MAX_X_COORDINATE: int = 50
 DEFAULT_MIN_Y_COORDINATE: int = -50
 DEFAULT_MAX_Y_COORDINATE: int = 50
 
+DEFAULT_MIN_MAX_X: str = MinMax(minValue=DEFAULT_MIN_X_COORDINATE, maxValue=DEFAULT_MAX_X_COORDINATE).__repr__()
+DEFAULT_MIN_MAX_Y: str = MinMax(minValue=DEFAULT_MIN_Y_COORDINATE, maxValue=DEFAULT_MAX_Y_COORDINATE).__repr__()
 
-# these are for the arrange method
-SECTION_ARRANGE: Section = Section(
-    [
-        ConfigurationNameValue(name=PropertyName('damping'),       defaultValue='0.5'),
-        ConfigurationNameValue(name=PropertyName('springLength'),  defaultValue='100'),
-        ConfigurationNameValue(name=PropertyName('maxIterations'), defaultValue='500'),
-        ConfigurationNameValue(name=PropertyName('attractionForce'), defaultValue='0.1'),
-        ConfigurationNameValue(name=PropertyName('repulsionForce'), defaultValue='10000'),
-    ]
-)
-
-# these randomize the layout
-SECTION_RANDOMIZE: Section = Section(
-    [
-        ConfigurationNameValue(name=PropertyName('minMaxX'), defaultValue=MinMax(minValue=DEFAULT_MIN_X_COORDINATE, maxValue=DEFAULT_MAX_X_COORDINATE).__repr__()),
-        ConfigurationNameValue(name=PropertyName('minMaxY'), defaultValue=MinMax(minValue=DEFAULT_MIN_Y_COORDINATE, maxValue=DEFAULT_MAX_Y_COORDINATE).__repr__()),
-    ]
+randomizeProperties: ValueDescriptions = ValueDescriptions(
+    {
+        KeyName('minMaxX'): ValueDescription(defaultValue=DEFAULT_MIN_MAX_X, deserializer=MinMax.deSerialize),
+        KeyName('minMaxY'): ValueDescription(defaultValue=DEFAULT_MIN_MAX_Y, deserializer=MinMax.deSerialize),
+    }
 )
 """
 Stop execution after this many number of iterations
 where the totalDisplacement is less that minimumTotalDisplacement
 """
-SECTION_EARLY_EXIT: Section = Section(
-    [
-        ConfigurationNameValue(name=PropertyName('minimumTotalDisplacement'), defaultValue='10'),
-        ConfigurationNameValue(name=PropertyName('stopCount'),                defaultValue='15'),
-    ]
+earlyExitProperties: ValueDescriptions = ValueDescriptions(
+    {
+        KeyName('minimumTotalDisplacement'): ValueDescription(defaultValue='10', deserializer=SecureConversions.secureInteger),
+        KeyName('stopCount'):                ValueDescription(defaultValue='15', deserializer=SecureConversions.secureInteger),
+    }
 )
-
-ARRANGE_SECTION_NAME:    SectionName = SectionName('Arrange')
-RANDOMIZE_SECTION_NAME:  SectionName = SectionName('Randomize')
-EARLY_EXIT_SECTION_NAME: SectionName = SectionName('EarlyExit')
-
 PYFDL_SECTIONS: Sections = Sections(
     {
-        ARRANGE_SECTION_NAME:    SECTION_ARRANGE,
-        RANDOMIZE_SECTION_NAME:  SECTION_RANDOMIZE,
-        EARLY_EXIT_SECTION_NAME: SECTION_EARLY_EXIT,
+        SectionName('Arrange'):   arrangeProperties,
+        SectionName('Randomize'): randomizeProperties,
+        SectionName('EarlyExit'): earlyExitProperties,
     }
 )
 
 
-class Configuration(ConfigurationProperties, metaclass=SingletonV3):
+class Configuration(DynamicConfiguration, metaclass=SingletonV3):
 
     def __init__(self):
+
+        self._logger: Logger = getLogger(__name__)
+
         super().__init__(baseFileName='pyfdl.ini', moduleName='pydfl', sections=PYFDL_SECTIONS)
-
-        self._loadConfiguration()
-
-    @property
-    @configurationGetter(sectionName=ARRANGE_SECTION_NAME, deserializeFunction=float)
-    def damping(self) -> float:
-        return 0.0
-
-    @damping.setter
-    @configurationSetter(sectionName=ARRANGE_SECTION_NAME)
-    def damping(self, newValue: float):
-        pass
-
-    @property
-    @configurationGetter(sectionName=ARRANGE_SECTION_NAME, deserializeFunction=int)
-    def springLength(self) -> int:
-        return 0
-
-    @springLength.setter
-    @configurationSetter(sectionName=ARRANGE_SECTION_NAME)
-    def springLength(self, newValue: int):
-        pass
-
-    @property
-    @configurationGetter(sectionName=ARRANGE_SECTION_NAME, deserializeFunction=int)
-    def maxIterations(self) -> int:
-        return 0
-
-    @maxIterations.setter
-    @configurationSetter(sectionName=ARRANGE_SECTION_NAME)
-    def maxIterations(self, newValue: int):
-        pass
-
-    @property
-    @configurationGetter(sectionName=ARRANGE_SECTION_NAME, deserializeFunction=float)
-    def attractionForce(self) -> float:
-        return 0.0
-
-    @attractionForce.setter
-    @configurationSetter(sectionName=ARRANGE_SECTION_NAME)
-    def attractionForce(self, newValue: float):
-        pass
-
-    @property
-    @configurationGetter(sectionName=ARRANGE_SECTION_NAME, deserializeFunction=int)
-    def repulsionForce(self) -> int:
-        return 0
-
-    @repulsionForce.setter
-    @configurationSetter(sectionName=ARRANGE_SECTION_NAME)
-    def repulsionForce(self, newValue: int):
-        pass
-
-    @property
-    @configurationGetter(sectionName=RANDOMIZE_SECTION_NAME, deserializeFunction=MinMax.deSerialize)
-    def minMaxX(self) -> MinMax:
-        return MinMax()
-
-    @minMaxX.setter
-    @configurationSetter(sectionName=RANDOMIZE_SECTION_NAME)
-    def minMaxX(self, newValue: MinMax):
-        pass
-
-    @property
-    @configurationGetter(sectionName=RANDOMIZE_SECTION_NAME, deserializeFunction=MinMax.deSerialize)
-    def minMaxY(self) -> MinMax:
-        return MinMax()
-
-    @minMaxY.setter
-    @configurationSetter(sectionName=RANDOMIZE_SECTION_NAME)
-    def minMaxY(self, newValue: MinMax):
-        pass
-
-    @property
-    @configurationGetter(sectionName=EARLY_EXIT_SECTION_NAME, deserializeFunction=int)
-    def minimumTotalDisplacement(self) -> int:
-        return 0
-
-    @minimumTotalDisplacement.setter
-    @configurationSetter(sectionName=EARLY_EXIT_SECTION_NAME)
-    def minimumTotalDisplacement(self, newValue: int):
-        pass
-
-    @property
-    @configurationGetter(sectionName=EARLY_EXIT_SECTION_NAME, deserializeFunction=int)
-    def stopCount(self) -> int:
-        return 0
-
-    @stopCount.setter
-    @configurationSetter(sectionName=EARLY_EXIT_SECTION_NAME)
-    def stopCount(self, newValue: int):
-        pass
