7820e68baaca54d6043a1e8b810f39282d01fc57
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2026-09-14 11:51:20 -0500

    <Feature>[UI]: <Add pyforcedirectedlayout.ui subpackage with ConfigurationPanel>
    [
    * Create pyforcedirectedlayout.ui subpackage with ConfigurationPanel and DlgConfiguration
    * ConfigurationPanel is single source of truth for all layout configuration controls
    * DlgConfiguration provides reusable OK/Cancel dialog wrapping ConfigurationPanel
    * Add ui optional-dependencies group with wxPython and codeallyadvanced
    * Refactor demo DlgConfiguration to delegate parameter UI to ConfigurationPanel
    * Update CI workflow to install ui extras
    ]
    
    [
    https://github.com/hasii2011/py-force-directed-layout/issues/22
    ]

---
+++ src/pyforcedirectedlayout/ui/DlgConfiguration.py
@@ -0,0 +1,67 @@
+
+from logging import Logger
+from logging import getLogger
+
+from wx import OK
+from wx import ID_OK
+from wx import CANCEL
+from wx import EVT_CLOSE
+from wx import EVT_BUTTON
+from wx import ID_CANCEL
+from wx import DEFAULT_DIALOG_STYLE
+
+from wx import Size
+from wx import Window
+from wx import CommandEvent
+from wx import StdDialogButtonSizer
+
+from wx.lib.sized_controls import SizedDialog
+from wx.lib.sized_controls import SizedPanel
+
+from pyforcedirectedlayout.ui.ConfigurationPanel import ConfigurationPanel
+
+
+class DlgConfiguration(SizedDialog):
+    """
+    A reusable dialog wrapping ConfigurationPanel with standard OK/Cancel buttons.
+    """
+
+    def __init__(self, parent: Window):
+
+        style:   int  = DEFAULT_DIALOG_STYLE
+        dlgSize: Size = Size(470, 540)
+
+        super().__init__(parent, title='Force Directed Configuration', size=dlgSize, style=style)
+
+        self.logger: Logger = getLogger(__name__)
+
+        sizedPanel: SizedPanel = self.GetContentsPane()
+        sizedPanel.SetSizerType('vertical')
+        sizedPanel.SetSizerProps(proportion=1)
+
+        self._configurationPanel: ConfigurationPanel = ConfigurationPanel(sizedPanel=sizedPanel)
+        self._layoutStandardOkCancelButtonSizer()
+
+    def _layoutStandardOkCancelButtonSizer(self):
+        """
+        Call this last when creating controls; Will take care of
+        adding callbacks for the Ok and Cancel buttons
+        """
+        buttSizer: StdDialogButtonSizer = self.CreateStdDialogButtonSizer(OK | CANCEL)
+
+        self.SetButtonSizer(buttSizer)
+        self.Bind(EVT_BUTTON, self._onOk,    id=ID_OK)
+        self.Bind(EVT_BUTTON, self._onClose, id=ID_CANCEL)
+        self.Bind(EVT_CLOSE,  self._onClose)
+
+    # noinspection PyUnusedLocal
+    def _onOk(self, _event: CommandEvent):
+        """
+        """
+        self.EndModal(OK)
+
+    # noinspection PyUnusedLocal
+    def _onClose(self, _event: CommandEvent):
+        """
+        """
+        self.EndModal(CANCEL)
